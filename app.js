7a3adaf563cc92de76999786d4b501e663206c9d
malversetech <malversetech@gmail.com>
2024-10-14 16:04:35 +0100

    adding files for PWA
    adding files for PWA

---
+++ app.js
@@ -0,0 +1,26 @@
+let deferredPrompt;
+const installBtn = document.getElementById('installBtn');
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  installBtn.hidden = false;
+
+  installBtn.addEventListener('click', () => {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+});
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then((reg) => console.log('Service Worker Registered', reg))
+    .catch((err) => console.log('Service Worker Registration Failed', err));
+}
